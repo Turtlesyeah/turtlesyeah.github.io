893ed9d97c0b70caff8e8445fa25f79f16fa062a
turtlesyeah <148290209+Turtlesyeah@users.noreply.github.com>
2025-02-14 07:27:08 -0600

    a

--- turtextular/recipes.js
+++ turtextular/recipes.js
@@ -1,1 +1,21 @@
+//creating the items
+item.main('icrium');
+item.main('bauxanium');
+item.main('idivium');
+item.main('virium');
+item.main('gregite');
+item.main('aluminite');
+item.main('pilium');
+item.main('idivite');
+item.main('bauxenite');
+item.main('metal_scrap');
+item.main('icrite');
+item.main('rust');
+//creating the fluids
+item.fluid('virite');
+item.fluid('bauxenite_fluid');
+item.fluid('liquidovite');
+
+
+//now, the smelting recipes
 recipe.smelting('metal_scrap', 'icrium')
