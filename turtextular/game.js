f870728f1e03a0d6ec4a78e4081f172a59c48125
turtlesyeah <148290209+Turtlesyeah@users.noreply.github.com>
2025-06-19 08:05:27 -0500

    always gotta remove debug

--- turtextular/game.js
+++ turtextular/game.js
@@ -387,7 +387,7 @@ update() {
         default: 'arcade',
         arcade: {
             gravity: { y: 600 },
-            debug: true // Enable debug to see collision boxes
+            debug: false // Enable debug to see collision boxes
         }
     },
     scene: [TitleScene, GameScene] 
